79f481f4098952b14a1cd02707c9be3a0970bf40
KacSzy <kacper.szymanski.22@gmail.com>
2024-03-11 13:16:14 +0100

    Zmiana z Ridera

--- Zajecia1/Zajecia1/Program.cs
+++ Zajecia1/Zajecia1/Program.cs
@@ -3,25 +3,6 @@
 public class Program
 {
     public static void Main(string[] args) {
-        //Console.WriteLine("Hello World!");
-
-        // 1. Primitive
-        int g = 10;
-        int g2 = g;
-
-        // 2. Complex
-        string s = "Ala";
-        string s2 = s;
-
-        int[] arr = { 4, 3, 2, 2 };
-        int[] arr2 = arr;
-        arr2[0] = 2222;
-
-        // -------
-
-        int age = 10;
-        //int? age -> mozna przypisac null
-        //int? age -> Nullable<int> age
-        //int ? class = null;
+        Console.WriteLine("Commit 2");
     }
 }

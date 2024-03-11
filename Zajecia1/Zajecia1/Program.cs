3585dcc3e3c31e7df6537ee1fdaa62b3c70d8176
KacSzy <kacper.szymanski.22@gmail.com>
2024-03-11 13:06:48 +0100

    Pierwsza wersja projektu

---
+++ Zajecia1/Zajecia1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main(string[] args) {
+        //Console.WriteLine("Hello World!");
+
+        // 1. Primitive
+        int g = 10;
+        int g2 = g;
+
+        // 2. Complex
+        string s = "Ala";
+        string s2 = s;
+
+        int[] arr = { 4, 3, 2, 2 };
+        int[] arr2 = arr;
+        arr2[0] = 2222;
+
+        // -------
+
+        int age = 10;
+        //int? age -> mozna przypisac null
+        //int? age -> Nullable<int> age
+        //int ? class = null;
+    }
+}
